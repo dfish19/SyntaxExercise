a5b31020ce1e38ca65424563b69e82b90372fb44
dfish19 <dfish19@yahoo.com>
2022-08-05 12:39:22 -0500

    initial

---
+++ SyntaxExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+    var answer = 4;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+Console.WriteLine(answer);
